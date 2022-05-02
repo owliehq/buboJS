d00804da1cba5525f6ca436f77f174570d84e02b
Arnaud Gerard <arnaud@owlie.xyz>
2022-05-02 10:23:03 +0200

    feat(root,api): jest config

---
+++ packages/api/jest.config.ts
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  displayName: 'api',
+  name: 'api',
+  rootDir: '../..',
+  testMatch: [`<rootDir>/packages/api/test/*.ts`],
+};
