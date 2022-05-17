81c25342a06e4cb1cf169cd1d1ad3c0610b8f145
Arnaud Gerard <arnaud@owlie.xyz>
2022-05-17 15:27:18 +0200

    feat(api): add routes method on adapter

--- packages/api/src/adapters/AdapterHttpModule.ts
+++ packages/api/src/adapters/AdapterHttpModule.ts
@@ -1,14 +1,25 @@
 import { Server } from 'http'
 
-export interface AdapterHttpModule {
+/**
+ * adapt http modules (tinyhttp, fastify, express) to main api module
+ */
+export interface AdapterHttpModule<App> {
   app: any
 
   init(): any
+  initRouter(): App
 
   startServer(): Server
   stopServer(): void
 
   get(path: string, handler: Function)
-  // post(path: string, handler: Function);
+  post(path: string, handler: Function)
+  put(path: string, handler: Function)
+  patch(path: string, handler: Function)
+  delete(path: string, handler: Function)
   // route(method: RouteMethod, path: string, handler: Function);
+
+  use(path: string, router: App)
+
+  listen(port: number)
 }
--- packages/tinyhttp/src/TinyHttpAdapter.ts
+++ packages/tinyhttp/src/TinyHttpAdapter.ts
@@ -1,24 +1,53 @@
-import { App } from '@tinyhttp/app';
-import { AdapterHttpModule } from '@bubojs/api';
+import { App } from '@tinyhttp/app'
+import { AdapterHttpModule } from '@bubojs/api'
+import { Server } from 'http'
 
-export class TinyHttpAdapter implements AdapterHttpModule {
-  public app: App;
+export class TinyHttpAdapter implements AdapterHttpModule<App> {
+  public app: App
   constructor() {
-    this.app = new App();
+    this.app = new App()
   }
 
   public init() {
-    return;
+    return
+  }
+
+  public initRouter() {
+    return new App()
   }
 
   public startServer() {
-    const server = this.app.listen(3000);
-    console.log('listened to 3000');
-    return server;
+    const server = this.app.listen(3000)
+    console.log('listened to 3000')
+    return server
   }
   public stopServer() {}
 
   public get(path: string, handler: any) {
-    this.app.get(path, handler);
+    return this.app.get(path, handler)
+  }
+
+  public post(path: string, handler: any) {
+    return this.app.post(path, handler)
+  }
+
+  public put(path: string, handler: any) {
+    return this.app.put(path, handler)
+  }
+
+  public patch(path: string, handler: any) {
+    return this.app.patch(path, handler)
+  }
+
+  public delete(path: string, handler: any) {
+    return this.app.delete(path, handler)
+  }
+
+  public use(path: string, router: App) {
+    return this.app.use(path, router)
+  }
+
+  public listen(port: number): Server {
+    return this.app.listen(port)
   }
 }
