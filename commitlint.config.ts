856e273febec6c9c77e6b769fafaea312238333a
Florian <florian@owlie.xyz>
2022-07-21 10:42:01 +0200

    fix(ci): wip

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -20,7 +20,7 @@ const Configuration: UserConfig = {
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-    'scope-enum': [2, 'always', ['root', 'api', 'tinyhttp', 'fastify', 'swagger', 'todolist', 'catalog',  'sequelize']],
+    'scope-enum': [2, 'always', ['root', 'api', 'tinyhttp', 'fastify', 'swagger', 'todolist', 'catalog',  'sequelize', 'ci']],
     'subject-max-length': [2, 'always', 120]
   }
   /*
