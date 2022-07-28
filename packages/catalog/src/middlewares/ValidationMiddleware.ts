39de56a75d3255363392047507abee380ca822dc
Florian <florian@owlie.xyz>
2022-07-28 12:03:45 +0200

    feat(catalog): refacto validation middleware

--- packages/catalog/src/middlewares/ValidationMiddleware.ts
+++ packages/catalog/src/middlewares/ValidationMiddleware.ts
@@ -16,21 +16,14 @@ export const validationMiddleware = (
 
     const check = MetadataManager.getRouteValidatorMetadata(controllerName, routeName)
 
-    let checkResult: Promise<true | ValidationError[]> | true | ValidationError[]
-    if (check.async) {
-      checkResult = await check(parameters)
-      let result = {}
-      await Promise.all(
-        Object.entries(parameters).map(async ([key, value]) => {
-          result[key] = await value
-        })
-      )
-      if (req.body) req.body = result
-      else if (req.query) req.query = result
-    } else {
-      checkResult = check(req.body ? req.body : req.query)
-    }
-
+    let checked = check(parameters)
+    let checkResult = checked instanceof Promise ? await checked : checked
+    let result = {}
+    Object.entries(parameters).map(([key, value]) => {
+      result[key] = value
+    })
+    if (req.body) req.body = result
+    else if (req.query) req.query = result
     if (checkResult && typeof checkResult === 'boolean') {
       next()
     } else {
@@ -38,13 +31,6 @@ export const validationMiddleware = (
     }
   }
   middlewares.push(handler)
-  // middlewares.push((req: any, res: any, next: Function) => {
-  //   console.log('add validator')
-  //   const v = new Validator(validatorOptions)
-  //   const check = v.compile(schema)
-  //   req.check = check
-  //   next()
-  // })
   if (beforeValidationMiddleware) middlewares.push(beforeValidationMiddleware)
 
   return middlewares
@@ -58,15 +44,15 @@ export interface ValidationData {
 
 export const ValidationMiddleware = (validationData: ValidationData) => {
   return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    // building validation checker
+    const { schema, validatorOptions } = validationData
+    const v = new Validator(validatorOptions)
+    const check = v.compile(schema)
+    MetadataManager.setRouteValidatorMetadata(target.constructor.name, propertyKey, check)
+
+    // push middleware to controller's middleware stack
     for (const middleware of validationMiddleware(validationData, target.constructor.name, propertyKey)) {
       MetadataManager.setMiddlewareMetadata(target.constructor.name, propertyKey, MiddlewarePosition.BEFORE, middleware)
     }
-
-    const { schema, validatorOptions } = validationData
-
-    const v = new Validator(validatorOptions)
-    const check = v.compile(schema)
-
-    MetadataManager.setRouteValidatorMetadata(target.constructor.name, propertyKey, check)
   }
 }
