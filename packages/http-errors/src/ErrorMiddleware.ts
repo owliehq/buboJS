5be10c845e0084ad84a13d125427f50e9be3b004
Florian <florian@owlie.xyz>
2022-09-15 10:40:19 +0200

    fix(http-errors): fixed bad behavior in http-errors middleware

--- packages/http-errors/src/ErrorMiddleware.ts
+++ packages/http-errors/src/ErrorMiddleware.ts
@@ -2,21 +2,21 @@ import { HttpError } from './HttpError'
 import { ErrorFactory, OptionsErrorFactory } from './ErrorFactory'
 
 export interface ErrorMiddlewareOptions {
-  debugClient: Boolean
-  debugServer: Boolean
+  debugOnClient: Boolean
+  debugOnServer: Boolean
   skipClientError: Boolean
 }
 
 export const errorsMiddleware = (options?: ErrorMiddlewareOptions) => {
   let opt: ErrorMiddlewareOptions = options || {
-    debugClient: false,
-    debugServer: false,
+    debugOnClient: false,
+    debugOnServer: false,
     skipClientError: false
   }
   const middleware: Function = (err: Error & HttpError, req, res, next) => {
     const statusCode = err.statusCode || 500
 
-    if (options.debugServer && ((!options.skipClientError && statusCode < 500) || statusCode >= 500)) console.log(err)
+    if (options.debugOnServer && ((!options.skipClientError && statusCode < 500) || statusCode >= 500)) console.log(err)
     if (!err.statusCode) {
       let options: OptionsErrorFactory = {
         message: err.message,
@@ -25,16 +25,17 @@ export const errorsMiddleware = (options?: ErrorMiddlewareOptions) => {
       err = ErrorFactory.InternalServerError(options)
     }
 
-    const stack = options.debugClient ? err.stack : undefined
+    const stack = options.debugOnClient ? err.stack : undefined
     const errorCode = err.errorCode ? 'E' + err.errorCode.toString().padStart(5, '0') : undefined
     const details = err.details || undefined
 
     res.status(statusCode).json({
       statusCode: err.statusCode,
-      errorCode: err.errorCode,
+      errorCode,
       message: err.message,
-      details: err.details,
-      stack: err.stack
-    })
+      details,
+      stack
+    } as any)
   }
+  return middleware
 }
